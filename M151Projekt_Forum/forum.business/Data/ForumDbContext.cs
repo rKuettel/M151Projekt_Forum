8f09a9c3e7149f2544f4f348ce0526e18bcdda08
André <andre.schreiber@roche.com>
2021-06-18 09:04:18 +0200

    add data folder

---
+++ M151Projekt_Forum/forum.business/Data/ForumDbContext.cs
@@ -0,0 +1,15 @@
+﻿using forum.business.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace forum.business.Data
+{
+    public class ForumDbContext : IdentityDbContext
+    {
+        public DbSet<Discussion> Discussions { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<PictureOnDatabaseModel> PicturesOnDatabase { get; set; }
+        public DbSet<PictureOnFileSystemModel> PicturesOnDatabase { get; set; }
+        public ForumDbContext(DbContextOptions<ForumDbContext> options) : base(options) { }
+    }
+}
