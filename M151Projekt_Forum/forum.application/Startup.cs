eb93647b4621e99f98fc171bfb8e3e6ecb1833c9
Roman Küttel <roman.kuettel@roche.com>
2021-06-25 09:04:40 +0200

    Updated Passwort Policy

--- M151Projekt_Forum/forum.application/Startup.cs
+++ M151Projekt_Forum/forum.application/Startup.cs
@@ -47,12 +47,12 @@ namespace forum.application
 
             services.Configure<IdentityOptions>(options =>
             {
-                options.Password.RequireDigit = false;
-                options.Password.RequireLowercase = false;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireUppercase = false;
-                options.Password.RequiredLength = 6;
-                options.Password.RequiredUniqueChars = 1;
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 10;
+                options.Password.RequiredUniqueChars = 3;
             });
 
 
